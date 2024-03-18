24a4592482e98ca7374a16c5256e449dd01319f0
anujHopingminds <anuj@hoopinminds.com>
2024-03-18 10:02:49 +0530

    15thMarch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import Poster from './components/Poster';
 import About from './components/About';
+import Menu from './components/Menu';
 function App() {
   return (
     <>
     <Header/>
     <Poster/>
     <About/>
+    <Menu/>
     </>
    
   );
