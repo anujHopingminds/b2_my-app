eb1303f8ae6353cf3680512be07f60b5d1d93693
anujHopingminds <anuj@hoopinminds.com>
2024-03-20 14:02:48 +0530

    20_march_api-intregate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 function Contact() {
-    const [name, setName] = useState();
+    const [name, setName] = useState('');
     const [num, setNum] = useState();
     const [date, setDate] = useState()
     const [message, setMessage] = useState()
@@ -23,7 +23,34 @@ function Contact() {
         setMessage(e.target.value)
     }
 
-    console.log(name, 'name')
+    const handleClear = (e) => {
+        e.preventDefault();
+        setName('');
+        setNum('');
+        setMessage('');
+        setDate('');
+    }
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        const data = {
+            name: name,
+            numPeople: num,
+            date: date,
+            message: message
+        };
+        try {
+            const response = await fetch('https://dummy.restapiexample.com/api/v1/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 
     return (
         <div className="container contact">
@@ -31,18 +58,19 @@ function Contact() {
             <p>We offer full-service catering for any event, large or small. We understand your needs and we will cater the food to satisfy the biggerst criteria of them all, both look and taste. Do not hesitate to contact us.</p>
             <p className="catering_test">Catering Service, 42nd Living St, 43043 New York, NY</p>
             <form>
-                <input onChange={handleChange} placeholder="name" type="test" />
+                <input onChange={handleChange} value={name} placeholder="name" type="test" />
                 {name}
                 <br />
-                <input onChange={handleNumChange} placeholder="How many people" />
+                <input onChange={handleNumChange} value={num} placeholder="How many people" />
                 {num}
                 <br />
-                <input onChange={hadleDate} placeholder="Date" type="datetime-local" />
+                <input onChange={hadleDate} value={date} placeholder="Date" type="datetime-local" />
                 {date}
                 <br />
-                <input onChange={handleMesChange} placeholder="Message \ Special requirements" type="test" />
+                <input onChange={handleMesChange} value={message} placeholder="Message \ Special requirements" type="test" />
                 {message}
-                <button >SEND MESSAGE</button>
+                <button onClick={handleClick} >SEND MESSAGE</button>
+                <button onClick={handleClear} >CLEAR MESSAGE</button>
             </form>
         </div>
     )
